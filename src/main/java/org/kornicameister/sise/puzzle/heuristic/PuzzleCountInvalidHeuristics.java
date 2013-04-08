db3a4c6c4de6b871ef0d180274db7980d566359a
kornicameister <kornicameister@gmail.com>
2013-04-08 23:03:53 +0200

    missing condition in PuzzleCountInvalidHeuristics.java - fixed and added heuristic cmp test

--- src/main/java/org/kornicameister/sise/puzzle/heuristic/PuzzleCountInvalidHeuristics.java
+++ src/main/java/org/kornicameister/sise/puzzle/heuristic/PuzzleCountInvalidHeuristics.java
@@ -8,6 +8,7 @@ import org.kornicameister.sise.puzzle.node.PuzzleNode;
 /**
  * Heuristic calculates invalid position between two Puzzle
  * return czount iinvalid position.
+ *
  * @author Seba
  * @version 0.0.1
  * @since 0.0.1
@@ -16,24 +17,28 @@ import org.kornicameister.sise.puzzle.node.PuzzleNode;
 
 public class PuzzleCountInvalidHeuristics implements Heuristic {
 
-	@Override
-	public Double heuristicValue(GraphNode start, GraphNode end) {
-		if (start instanceof PuzzleNode && end instanceof PuzzleNode) {
-			PuzzleNode startPuzzleNode = (PuzzleNode) start,
-					puzzleEndNode = (PuzzleNode) end;
-		double iloscNiepoprawnych = 0;
-	
-		for(int i=0;i<startPuzzleNode.getPuzzle().length;i++){
-			for(int j=0;j<startPuzzleNode.getPuzzle()[i].length;j++){
-				if(startPuzzleNode.getPuzzle()[i][j]!=puzzleEndNode.getPuzzle()[i][j]) iloscNiepoprawnych=iloscNiepoprawnych+1;
-			}
-		}
-				
+    @Override
+    public Double heuristicValue(GraphNode start, GraphNode end) {
+        if (start instanceof PuzzleNode && end instanceof PuzzleNode) {
+            PuzzleNode startPuzzleNode = (PuzzleNode) start,
+                    puzzleEndNode = (PuzzleNode) end;
+            double invalidCount = 0;
 
-		return iloscNiepoprawnych;
+            for (int i = 0; i < startPuzzleNode.getPuzzle().length; i++) {
+                for (int j = 0; j < startPuzzleNode.getPuzzle()[i].length; j++) {
+                    final Integer integer = startPuzzleNode.getPuzzle()[i][j];
+                    final Integer integer1 = puzzleEndNode.getPuzzle()[i][j];
+                    if (!integer.equals(integer1) && (!integer.equals(0) && integer1.equals(0))) {
+                        invalidCount += 1.0;
+                    }
+                }
+            }
 
-		}
-		return null;
-	}
+
+            return invalidCount;
+
+        }
+        return null;
+    }
 
 }
--- src/test/java/org/kornicameister/sise/puzzle/stratagies/AStarPuzzleStrategyTest.java
+++ src/test/java/org/kornicameister/sise/puzzle/stratagies/AStarPuzzleStrategyTest.java
@@ -5,12 +5,14 @@ import org.junit.Test;
 import org.kornicameister.sise.core.Graph;
 import org.kornicameister.sise.puzzle.AbstractPuzzleTest;
 import org.kornicameister.sise.puzzle.PuzzleSolverImpl;
+import org.kornicameister.sise.puzzle.heuristic.PuzzleCountInvalidHeuristics;
 import org.kornicameister.sise.puzzle.heuristic.PuzzleManhattanHeuristic;
 import org.kornicameister.sise.puzzle.node.PuzzleNode;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import static org.hamcrest.CoreMatchers.is;
 
@@ -26,7 +28,7 @@ public class AStarPuzzleStrategyTest extends AbstractPuzzleTest {
         Integer key = -1;
         int counter = 0;
         String[] order = {"L", "P", "G", "D"};
-        while (!key.equals(21)) {                // BY-FAR currently acceptable computation time
+        while (!key.equals(10)) {                // BY-FAR currently acceptable computation time
             final List<Integer[][]> integers = this.puzzleMap.get(key);
             for (Integer[][] puzzle : integers) {
 
@@ -59,7 +61,7 @@ public class AStarPuzzleStrategyTest extends AbstractPuzzleTest {
 
     @Test
     public void testAStrategyWithRandomOrder() {
-        Integer key = 0;
+        Integer key = -1;
         int counter = 0;
         while (!key.equals(11)) {                // BY-FAR currently acceptable computation time
             final List<Integer[][]> integers = this.puzzleMap.get(key);
@@ -83,5 +85,93 @@ public class AStarPuzzleStrategyTest extends AbstractPuzzleTest {
             counter = 0;
             key++;
         }
+    }
+
+    @Test
+    public void testAStrategyWithFixedOrderInvalidHeuristic() {
+        Integer key = 0;
+        int counter = 0;
+        String[] order = {"L", "P", "G", "D"};
+        while (!key.equals(7)) {                // BY-FAR currently acceptable computation time
+            final List<Integer[][]> integers = this.puzzleMap.get(key);
+            for (Integer[][] puzzle : integers) {
+
+                // generate order for this pass
+                Collections.shuffle(Arrays.asList(order));
+                StringBuilder sb = new StringBuilder();
+                for (String anOrder : order) {
+                    sb.append(anOrder);
+                }
+
+                System.out.println(String.format("Solving %s at [%d,%d,%s]", Arrays.deepToString(puzzle), key, counter++, sb.toString()));
+                Graph graph = new Graph((new PuzzleNode("Init", sb.toString(), puzzle)));
+                graph.setStrategy(new AStarPuzzleStrategy(new PuzzleCountInvalidHeuristics()));
+
+                PuzzleSolverImpl puzzleSolverImpl = new PuzzleSolverImpl(graph);
+                puzzleSolverImpl.setExamination(new InversionAccessibleNodeStrategy());
+
+                Assert.assertThat(puzzleSolverImpl.isSolvable(), is(true));
+                puzzleSolverImpl.solve();
+                if (graph.getPath() == null) {
+                    System.out.println("Failed to locate solution");
+                } else {
+                    System.out.println(graph.getStrategy().getReport());
+                }
+            }
+            counter = 0;
+            key++;
+        }
+    }
+
+    @Test
+    public void testAStrategyWithRandomOrderInvalidHeuristic() {
+        Integer key = 0;
+        int counter = 0;
+        while (!key.equals(7)) {                // BY-FAR currently acceptable computation time
+            final List<Integer[][]> integers = this.puzzleMap.get(key);
+            for (Integer[][] puzzle : integers) {
+
+                System.out.println(String.format("Solving %s at [%d,%d,%s]", Arrays.deepToString(puzzle), key, counter++, "R"));
+                Graph graph = new Graph((new PuzzleNode("Init", "R", puzzle)));
+                graph.setStrategy(new AStarPuzzleStrategy(new PuzzleCountInvalidHeuristics()));
+
+                PuzzleSolverImpl puzzleSolverImpl = new PuzzleSolverImpl(graph);
+                puzzleSolverImpl.setExamination(new InversionAccessibleNodeStrategy());
+
+                Assert.assertThat(puzzleSolverImpl.isSolvable(), is(true));
+                puzzleSolverImpl.solve();
+                if (graph.getPath() == null) {
+                    System.out.println("Failed to locate solution");
+                } else {
+                    System.out.println(graph.getStrategy().getReport());
+                }
+            }
+            counter = 0;
+            key++;
+        }
+    }
+
+    @Test
+    public void testCompareHeuristics() {
+        Integer key = 10;
+        final List<Integer[][]> integers = this.puzzleMap.get(key);
+        final int i = new Random().nextInt(integers.size());
+        Integer[][] puzzle = integers.get(i);
+
+
+        System.out.println(String.format("Comparing heuristics %s at [%d,%d,%s]", Arrays.deepToString(puzzle), key, i, "R"));
+        Graph graph = new Graph((new PuzzleNode("Init", "R", puzzle)));
+        PuzzleSolverImpl puzzleSolverImpl = new PuzzleSolverImpl(graph);
+        puzzleSolverImpl.setExamination(new InversionAccessibleNodeStrategy());
+        Assert.assertThat(puzzleSolverImpl.isSolvable(), is(true));
+
+        graph.setStrategy(new AStarPuzzleStrategy(new PuzzleCountInvalidHeuristics()));
+        puzzleSolverImpl.solve();
+        System.out.println(graph.getStrategy().getReport());
+
+        graph.setStrategy(new AStarPuzzleStrategy(new PuzzleManhattanHeuristic()));
+        puzzleSolverImpl.solve();
+        System.out.println(graph.getStrategy().getReport());
+
     }
 }
